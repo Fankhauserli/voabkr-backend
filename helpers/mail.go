1d9fba1344f13438e883d64aacecf9fc7e2b8fa1
Linus Fankhauser <linus.fankhauser@proton.me>
2026-09-17 16:15:07 +0900

    feat: implement email verification functionality and add package declarations for types

--- helpers/mail.go
+++ helpers/mail.go
@@ -3,10 +3,13 @@ package helpers
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"net/smtp"
+	"os"
 	"time"
 
 	"github.com/Fankhauserli/voabkr-backend/sql/db"
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 func generateSecureToken(length int) string {
@@ -17,15 +20,27 @@ func generateSecureToken(length int) string {
 	return hex.EncodeToString(b)
 }
 
-func SendVerificationEmail(c *gin.Context, db *db.Queries, userID int, email string) error {
+func SendVerificationEmail(c *gin.Context, database *db.Queries, userID int, email string) error {
 
 	token := generateSecureToken(64)
 
-	db.CreateVerificationToken(c.Request.Context(), db.CreateVerificationTokenParams{
+	database.CreateVerificationToken(c.Request.Context(), db.CreateVerificationTokenParams{
 		UserID:    int64(userID),
-		Token:     token,                                                            // You should generate a secure token here
-		ExpiresAt: c.Request.Context().Value("now").(time.Time).Add(24 * time.Hour), // Token expires in 24 hours
+		Token:     token,                                                                                                   // You should generate a secure token here
+		ExpiresAt: pgtype.Timestamptz{Time: c.Request.Context().Value("now").(time.Time).Add(24 * time.Hour), Valid: true}, // Token expires in 24 hours
 	})
+
+	SMTTPAddr := os.Getenv("SMTP_ADDR")
+	SMTPUser := os.Getenv("SMTP_USER")
+	SMTPPass := os.Getenv("SMTP_PASS")
+	FromEmail := os.Getenv("FROM_EMAIL")
+
+	smtpAuth := smtp.PlainAuth("", SMTPUser, SMTPPass, SMTTPAddr)
+	if SMTPUser == "" || SMTPPass == "" {
+		smtpAuth = nil // No authentication if credentials are not provided
+	}
+
+	smtp.SendMail(SMTTPAddr, smtpAuth, FromEmail, []string{email}, []byte("Verification email content, including the token: "+token))
 
 	return nil
 }
